4ee6993af0d6c095f878ce931aed7aa03a04b26c
Scott Ouellette <scottx611x@gmail.com>
2016-11-29 17:18:34 -0500

    update pagination

--- bsm_samples/urls.py
+++ bsm_samples/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import include, url
 from bsct.urls import URLGenerator
 from . import models, views
 
-project_patterns = URLGenerator(models.Project).get_urlpatterns(paginate_by=3)
-sample_patterns = URLGenerator(models.Sample).get_urlpatterns(paginate_by=3)
+project_patterns = URLGenerator(models.Project).get_urlpatterns(paginate_by=10)
+sample_patterns = URLGenerator(models.Sample).get_urlpatterns(paginate_by=10)
 sequencing_center_patterns = URLGenerator(
-    models.SequencingCenter).get_urlpatterns(paginate_by=3)
+    models.SequencingCenter).get_urlpatterns(paginate_by=10)
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
