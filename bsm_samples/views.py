64a53d78fffb881c2e54f666ab2e2ffcd4968ec3
Scott Ouellette <scottx611x@gmail.com>
2016-11-29 15:01:08 -0500

    test

--- bsm_samples/views.py
+++ bsm_samples/views.py
@@ -13,4 +13,4 @@ def search(request):
 
         search_results = watson.search(search_query)
 
-        return render(request, 'base.html', context={"SEARCH_RESULTS": search_results})
+        return render(request, 'base.html', context={"SEARCH_RESULTS": ["dog", "cat", "monkey"]})
