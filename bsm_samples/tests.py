c17f46d085d9b46a958aecef10cb7568561a4bd4
Scott Ouellette <scottx611x@gmail.com>
2016-11-29 19:48:25 -0500

    Add a test and travis.yml

--- bsm_samples/tests.py
+++ bsm_samples/tests.py
@@ -1,3 +1,14 @@
+from django.db import IntegrityError
 from django.test import TestCase
+from .models import Project
 
-# Create your tests here.
+
+class ProjectTestCase(TestCase):
+
+    def setUp(self):
+        Project.objects.create(name="Test Project")
+
+    def test_verify_unique_name(self):
+        """Projects should always have a unique name"""
+        self.assertRaises(
+            IntegrityError, Project.objects.create, name="Test Project")
