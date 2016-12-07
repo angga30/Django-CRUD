d6b3d68fb2a4eb042f2a3461f9fc959f2df1b6c1
Scott Ouellette <scottx611x@gmail.com>
2016-12-07 01:27:06 -0500

    😡

--- selenium_tests/test_selenium.py
+++ selenium_tests/test_selenium.py
@@ -1,6 +1,5 @@
 import os
 import pytest
-from urlparse import urljoin
 from .utils import assert_body_text
 
 base_url = os.environ['BASE_URL']
@@ -19,32 +18,3 @@ def test_base(selenium):
         selenium, 'BSM Web', 'Data',
         "This is a Proof of Concept Data Management app for the Brain "
         "Somatic Mosaicism project and its collaborators.")
-
-
-def test_search_expect_results(selenium):
-    selenium.get(urljoin(base_url, "sequencingcenter/create"))
-
-    # Create a Sequencing Center
-    seq_center_name = "Test Sequencing Center"
-    selenium.find_element_by_id('id_name').send_keys(seq_center_name)
-    selenium.find_element_by_xpath('//input[@type="submit"]').click()
-
-    selenium.get(base_url)
-
-    search_term = "Sequencing"
-    selenium.find_element_by_id('search_input').send_keys(search_term)
-
-    assert_body_text(
-        selenium,
-        "SequencingCenter: {}".format(seq_center_name),
-        "Showing 1 to 1 of 1 entries",
-    )
-
-
-def test_search_expect_no_results(selenium):
-    selenium.get(base_url)
-
-    search_term = "Coffee is tasty"
-    selenium.find_element_by_id('search_input').send_keys(search_term)
-
-    assert_body_text(selenium, 'No results found for: {}'.format(search_term))
