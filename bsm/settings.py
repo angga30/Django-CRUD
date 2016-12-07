486744e96e1f2af19465334375a3939460e9a1dc
Scott Ouellette <scottx611x@gmail.com>
2016-12-07 01:54:48 -0500

    Band aid

--- bsm/settings.py
+++ bsm/settings.py
@@ -155,5 +155,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Use django-nose as a test runner
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
