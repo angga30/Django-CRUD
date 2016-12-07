4cd20d9ff8b291180d1aa9694b64aadc8be2d1b1
Scott Ouellette <scottx611x@gmail.com>
2016-12-07 01:22:17 -0500

    Update travis

--- selenium_tests/test_selenium.py
+++ selenium_tests/test_selenium.py
@@ -16,8 +16,7 @@ def test_base(selenium):
     selenium.get(base_url)
 
     assert_body_text(
-        selenium, 'BSM Web', 'Data', 'Case Numbers', 'Diagnoses',
-        'Projects', 'Samples', 'Sequencing Centers',
+        selenium, 'BSM Web', 'Data',
         "This is a Proof of Concept Data Management app for the Brain "
         "Somatic Mosaicism project and its collaborators.")
 
