f04711b0521f324eb33b9b73eef8943bec635ed6
Scott Ouellette <scottx611x@gmail.com>
2016-12-07 02:25:44 -0500

    test

--- bsm/settings.py
+++ bsm/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
